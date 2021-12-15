df8890d8c872a0119352535fbdd4f2df8fbb4cf9
Emil Privér <emil@rivercode.se>
2021-12-15 18:57:08 +0100

    Added query parameters

--- src/handler.ts
+++ src/handler.ts
@@ -2,8 +2,13 @@ import { getAccessToken } from './lib/spotify'
 
 export async function handleRequest(request: Request): Promise<Response> {
   const { access_token } = await getAccessToken();
+  const url = new URL(request.url)
 
-  let response = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term', {
+  const time_range = url.searchParams.get('time_range') ?? 'short_term'
+  const limit = url.searchParams.get('time_range') ?? '20'
+  const offset = url.searchParams.get('time_range') ?? '0'
+
+  let response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=${time_range}&limit=${limit}&offset=${offset}`, {
     cf: {
       cacheTtl: 3600,
       cacheEverything: true,
