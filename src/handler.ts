719c3a5eaf9477c1ba5a085d693de227cc69987c
Emil Privér <emil@rivercode.se>
2022-01-30 18:39:03 +0100

    Added type to url to be able to switch between artists and types

--- src/handler.ts
+++ src/handler.ts
@@ -4,11 +4,12 @@ export async function handleRequest(request: Request): Promise<Response> {
   const { access_token } = await getAccessToken();
   const url = new URL(request.url)
 
+  const type = url.searchParams.get('type') ?? 'tracks'
   const time_range = url.searchParams.get('time_range') ?? 'short_term'
   const limit = url.searchParams.get('limit') ?? '20'
   const offset = url.searchParams.get('offset') ?? '0'
 
-  let response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=${time_range}&limit=${limit}&offset=${offset}`, {
+  let response = await fetch(`https://api.spotify.com/v1/me/top/${type}?time_range=${time_range}&limit=${limit}&offset=${offset}`, {
     cf: {
       cacheTtl: 3600,
       cacheEverything: true,
