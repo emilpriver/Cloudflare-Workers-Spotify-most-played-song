b4904e062b38fdfb0427189738446d36d3dff822
Emil Privér <emil@rivercode.se>
2022-01-31 21:51:51 +0100

    Moved top played to /top

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-import { handleRequest } from './handler'
+import {getAccessToken} from './lib/spotify'
+import {Router} from "itty-router";
 
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+const router = Router()
+
+router.get("/top", async request => {
+  const {access_token} = await getAccessToken();
+  const url = new URL(request.url)
+
+  const type = url.searchParams.get('type') ?? 'tracks'
+  const time_range = url.searchParams.get('time_range') ?? 'short_term'
+  const limit = url.searchParams.get('limit') ?? '20'
+  const offset = url.searchParams.get('offset') ?? '0'
+
+  const data = await fetch(`https://api.spotify.com/v1/me/top/${type}?time_range=${time_range}&limit=${limit}&offset=${offset}`, {
+    cf: {
+      cacheTtl: 3600,
+      cacheEverything: true,
+    },
+    headers: {
+      Authorization: `Bearer ${access_token}`
+    }
+  })
+      .then((r) => r.json())
+
+  return new Response(JSON.stringify(data), {
+    headers: {'content-type': 'application/json'},
+  })
+})
+
+router.all('*', () => new Response('Not Found.', {status: 404}))
+
+addEventListener('fetch', event => {
+  event.respondWith(router.handle(event.request))
 })
